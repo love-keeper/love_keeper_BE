af24994f0038af259597335a1ba25257a937d942
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-06 17:13:53 +0900

    🦺 [#0] valid(Member): 이메일 중복 검증 로직 추가
    이메일 검증 로직 추가

--- src/main/java/com/example/lovekeeper/domain/member/exception/annotation/UniqueEmail.java
+++ src/main/java/com/example/lovekeeper/domain/member/exception/annotation/UniqueEmail.java
@@ -1,2 +1,29 @@
-package com.example.lovekeeper.domain.member.exception.annotation;public @interface UniqueEmail {
+package com.example.lovekeeper.domain.member.exception.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.example.lovekeeper.global.validator.UniqueEmailValidator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+/**
+ * 이메일 중복 검증을 위한 커스텀 어노테이션
+ */
+@Documented
+@Constraint(validatedBy = UniqueEmailValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})  // @RequestParam에 직접 붙이거나, DTO의 필드에 붙일 때 사용
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueEmail {
+
+	String message() default "이미 사용 중인 이메일입니다.";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
 }
+
