78fe55a9b199a682a58c334d4ccdcb74baf65f5d
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-08 00:50:39 +0900

    ✨ [#5] feature(Draft): 편지 임시 저장 기능
    편지를 임시 저장하고 읽는 기능을 개발함

---
+++ src/main/java/com/example/lovekeeper/domain/draft/model/Draft.java
@@ -0,0 +1,65 @@
+package com.example.lovekeeper.domain.draft.model;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.example.lovekeeper.domain.member.model.Member;
+import com.example.lovekeeper.global.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "draft")
+public class Draft extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "draft_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	private int draftOrder;
+
+	@Lob
+	private String content;
+
+	//== 생성 메서드 ==//
+	public static Draft createDraft(Member member, int order, String content) {
+		return Draft.builder()
+			.member(member)
+			.draftOrder(order)
+			.content(content)
+			.build();
+	}
+
+	//== 연관 관계 메서드 ==//
+
+	//== 비즈니스 로직 ==//
+	public void updateContent(String content) {
+		this.content = content;
+	}
+
+}
