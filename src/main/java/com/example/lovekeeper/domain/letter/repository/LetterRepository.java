28237bada4eff66558ba18801da680fdcade80d6
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-13 18:02:41 +0900

    ✨ [#13] feature(Letter): 커플이 주고 받은 편지 개수 조회 API
    커플이 주고 받은 편지의 개수가 몇개인지 개수를 리턴하는 API를 개발함

--- src/main/java/com/example/lovekeeper/domain/letter/repository/LetterRepository.java
+++ src/main/java/com/example/lovekeeper/domain/letter/repository/LetterRepository.java
@@ -3,6 +3,8 @@ package com.example.lovekeeper.domain.letter.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.lovekeeper.domain.letter.model.Letter;
 import com.example.lovekeeper.domain.member.model.Member;
@@ -12,4 +14,6 @@ public interface LetterRepository extends JpaRepository<Letter, Long> {
 	// 편지 목록을 보낸 사람(sender) 또는 받은 사람(receiver) 기준으로 조회
 	Slice<Letter> findBySenderOrReceiver(Member sender, Member receiver, Pageable pageable);
 
+	@Query("select count(l) from Letter l where l.couple.id = :coupleId")
+	long countByCoupleId(@Param("coupleId") Long coupleId);
 }
