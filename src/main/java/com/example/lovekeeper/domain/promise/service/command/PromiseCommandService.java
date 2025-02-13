de33b185ec11ca545818d334e98252854372480d
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-13 16:56:53 +0900

    ✨ [#11] feature(Promise): 약속 삭제
    약속 삭제 API 구현

--- src/main/java/com/example/lovekeeper/domain/promise/service/command/PromiseCommandService.java
+++ src/main/java/com/example/lovekeeper/domain/promise/service/command/PromiseCommandService.java
@@ -3,5 +3,6 @@ package com.example.lovekeeper.domain.promise.service.command;
 public interface PromiseCommandService {
 
 	void createPromise(Long memberId, String content);
-	
+
+	void deletePromise(Long memberId, Long promiseId);
 }
