513d7f3511070d838f662fb5a6868411917fe036
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-14 00:02:01 +0900

    ✨ [#15] feature(Member): logout
    logout

--- src/main/java/com/example/lovekeeper/domain/member/model/Member.java
+++ src/main/java/com/example/lovekeeper/domain/member/model/Member.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import com.example.lovekeeper.domain.auth.model.RefreshToken;
 import com.example.lovekeeper.domain.connectionhistory.model.ConnectionHistory;
 import com.example.lovekeeper.domain.couple.model.CoupleStatus;
 import com.example.lovekeeper.domain.draft.model.Draft;
@@ -20,7 +19,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -47,9 +45,6 @@ public class Member extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "member_id")
 	private Long id;
-
-	@OneToOne(fetch = FetchType.LAZY, mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-	private RefreshToken refreshToken;
 
 	@Builder.Default
 	@OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
