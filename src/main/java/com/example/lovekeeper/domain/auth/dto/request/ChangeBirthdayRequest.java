6e5ce09545385b8dd339b417ccc85f2536f820b0
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-13 23:32:29 +0900

    ✨ [#15] feature(Member): 비밀번호 초기화 이후 변경하는 API
    비밀번호를 초기화하고 그 후에 비밀번호를 변경하는 API.

--- src/main/java/com/example/lovekeeper/domain/auth/dto/request/ChangeBirthdayRequest.java
+++ src/main/java/com/example/lovekeeper/domain/auth/dto/request/ChangeBirthdayRequest.java
@@ -3,6 +3,7 @@ package com.example.lovekeeper.domain.auth.dto.request;
 import java.time.LocalDate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import lombok.Getter;
 
@@ -12,6 +13,7 @@ public class ChangeBirthdayRequest {
 
 	@Schema(description = "변경할 생일", example = "1999-01-01")
 	@Past(message = "과거의 날짜만 입력 가능합니다.")
+	@NotBlank(message = "생일을 입력해주세요.")
 	private LocalDate birthday;
 
 }
