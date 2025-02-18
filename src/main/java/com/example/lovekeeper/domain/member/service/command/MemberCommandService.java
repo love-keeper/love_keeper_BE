2b388b85339b2b4b04cbb1642533a57d40129e84
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-02-18 14:59:34 +0900

    ✨ [#23] feature(Swagger): 스웨거 설정

--- src/main/java/com/example/lovekeeper/domain/member/service/command/MemberCommandService.java
+++ src/main/java/com/example/lovekeeper/domain/member/service/command/MemberCommandService.java
@@ -1,8 +1,19 @@
 package com.example.lovekeeper.domain.member.service.command;
 
+import java.time.LocalDate;
+
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.lovekeeper.domain.auth.dto.request.ChangePasswordRequest;
+import com.example.lovekeeper.domain.auth.dto.response.ChangeBirthdayResponse;
+import com.example.lovekeeper.domain.auth.dto.response.ChangeNicknameResponse;
+
 public interface MemberCommandService {
+	/**
+	 * 비밀번호 변경
+	 */
+	void changePassword(Long memberId, ChangePasswordRequest request);
+
 	/**
 	 * 회원 탈퇴
 	 * @param memberId 탈퇴할 회원의 ID
@@ -10,4 +21,9 @@ public interface MemberCommandService {
 	void withdrawMember(Long memberId);
 
 	void updateProfileImage(Long memberId, MultipartFile profileImage);
+
+	ChangeNicknameResponse changeNickname(Long memberId, String nickname);
+
+	ChangeBirthdayResponse changeBirthday(Long memberId, LocalDate birthday);
+
 }
