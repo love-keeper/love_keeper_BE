7adc36b17b0e6ebc7d9e09720c90eec2b4d0ecdd
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2025-03-01 21:16:03 +0900

    ✨ [#0] feature(Calendar): 캘린더 기능 추가

--- src/main/java/com/example/lovekeeper/domain/calendar/service/query/CalendarQueryService.java
+++ src/main/java/com/example/lovekeeper/domain/calendar/service/query/CalendarQueryService.java
@@ -1,7 +1,8 @@
+// CalendarQueryService 인터페이스
 package com.example.lovekeeper.domain.calendar.service.query;
 
 import com.example.lovekeeper.domain.calendar.dto.response.CalendarResponse;
 
 public interface CalendarQueryService {
-	CalendarResponse getCalendarDataForMember(Long memberId, int year, int month);
+	CalendarResponse getCalendarDataForMember(Long memberId, int year, int month, Integer day);
 }
