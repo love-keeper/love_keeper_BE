7b571514e136f4e7ed6597f7efcd67dad7be3e1f
dong99u <qkrehdrb0813@gmail.com>
2025-03-20 18:24:45 +0900

    draft type 추가

--- src/main/java/com/example/lovekeeper/domain/draft/repository/DraftRepository.java
+++ src/main/java/com/example/lovekeeper/domain/draft/repository/DraftRepository.java
@@ -3,9 +3,10 @@ package com.example.lovekeeper.domain.draft.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.lovekeeper.domain.draft.model.Draft;
+import com.example.lovekeeper.domain.draft.model.DraftType;
 
 public interface DraftRepository extends JpaRepository<Draft, Long> {
 
-	Draft findByMemberIdAndDraftOrder(Long memberId, int draftOrder);
+	Draft findByMemberIdAndDraftOrderAndDraftType(Long memberId, int draftOrder, DraftType draftType);
 
 }
