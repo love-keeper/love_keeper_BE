11320023f06df3659a0e4d3a8a496fa2dd07f88e
dong99u <qkrehdrb0813@gmail.com>
2025-03-20 18:24:45 +0900

    draft type 추가

--- src/main/java/com/example/lovekeeper/domain/draft/service/query/DraftQueryServiceImpl.java
+++ src/main/java/com/example/lovekeeper/domain/draft/service/query/DraftQueryServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.lovekeeper.domain.draft.dto.response.DraftResponse;
 import com.example.lovekeeper.domain.draft.exception.DraftErrorStatus;
 import com.example.lovekeeper.domain.draft.exception.DraftException;
 import com.example.lovekeeper.domain.draft.model.Draft;
+import com.example.lovekeeper.domain.draft.model.DraftType;
 import com.example.lovekeeper.domain.draft.repository.DraftRepository;
 import com.example.lovekeeper.domain.member.exception.MemberErrorStatus;
 import com.example.lovekeeper.domain.member.exception.MemberException;
@@ -25,7 +26,7 @@ public class DraftQueryServiceImpl implements DraftQueryService {
 	private final MemberRepository memberRepository;
 
 	@Override
-	public DraftResponse getDraft(Long memberId, int order) {
+	public DraftResponse getDraft(Long memberId, int order, String draftType) {
 		log.info("getDraft memberId: {}, order: {}", memberId, order);
 
 		if (order < 1 || order > 4) {
@@ -36,7 +37,8 @@ public class DraftQueryServiceImpl implements DraftQueryService {
 		memberRepository.findById(memberId)
 			.orElseThrow(() -> new MemberException(MemberErrorStatus.MEMBER_NOT_FOUND));
 
-		Draft draft = draftRepository.findByMemberIdAndDraftOrder(memberId, order);
+		Draft draft = draftRepository.findByMemberIdAndDraftOrderAndDraftType(memberId, order,
+			DraftType.valueOf(draftType));
 
 		if (draft == null) {
 			throw new DraftException(DraftErrorStatus.DRAFT_NOT_FOUND);
