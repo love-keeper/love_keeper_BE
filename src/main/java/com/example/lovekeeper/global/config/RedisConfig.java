20323667d9f33ca6b8942ed94e1a25197231e74b
dong99u <qkrehdrb0813@gmail.com>
2025-03-27 17:41:37 +0900

    💚 Redis 이름 localhost로 통일

--- src/main/java/com/example/lovekeeper/global/config/RedisConfig.java
+++ src/main/java/com/example/lovekeeper/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.lovekeeper.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -13,16 +14,20 @@ import io.lettuce.core.ReadFrom;
 @Configuration
 public class RedisConfig {
 
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
 	@Bean
 	public LettuceConnectionFactory redisConnectionFactory() {
-		// Redis 단일 인스턴스 설정
 		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-		redisConfig.setHostName("lovekeeper-redis"); // Docker 컨테이너 이름
-		redisConfig.setPort(6379);                   // Docker 매핑 포트
+		redisConfig.setHostName(redisHost); // "localhost" from application.yml or env
+		redisConfig.setPort(redisPort);
 
-		// Lettuce 클라이언트 설정: 마스터 노드에서만 읽기/쓰기
 		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-			.readFrom(ReadFrom.MASTER)           // 마스터에서만 동작하도록 강제
+			.readFrom(ReadFrom.MASTER)
 			.build();
 
 		return new LettuceConnectionFactory(redisConfig, clientConfig);
@@ -32,7 +37,6 @@ public class RedisConfig {
 	public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
-		// 키와 값을 직렬화 방식 설정 (문자열로 처리)
 		template.setKeySerializer(new StringRedisSerializer());
 		template.setValueSerializer(new StringRedisSerializer());
 		template.afterPropertiesSet();
