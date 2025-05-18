fa3df0ef2d2686590c77af48334f22f6468bcb1c
stophyeon <jj6778@naver.com>
2025-05-18 15:46:28 +0900

    날짜 형식 변경 API 요청 = LocalDate -> String 변환 = String -> LocalDateTime

--- src/main/java/com/example/lovekeeper/domain/letter/service/query/LetterQueryServiceImpl.java
+++ src/main/java/com/example/lovekeeper/domain/letter/service/query/LetterQueryServiceImpl.java
@@ -87,11 +87,9 @@ public class LetterQueryServiceImpl implements LetterQueryService {
 	public LetterResponse.LetterListResponse getLettersByDate(Long memberId, String date, int page, int size) {
 		LocalDateTime dt = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
 		Pageable pageable = PageRequest.of(page, size, Sort.by("sentDate", "createdAt").descending());
-
 		// 커플 정보 조회
 		Couple couple = coupleRepository.findByMemberId(memberId)
 			.orElseThrow(() -> new CoupleException(CoupleErrorStatus.COUPLE_NOT_FOUND));
-
 		// 특정 날짜 편지 조회
 		Slice<Letter> letters = letterRepository.findByCoupleIdAndSentDate(couple.getId(), dt, pageable);
 		return LetterResponse.LetterListResponse.from(letters);
